75117aa2cfb5a10de61563c231c2d3d9b42e52bb
quiqus <henrix427@gmail.com>
2023-12-04 20:26:25 +0100

    recover point

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,5 +1,30 @@
 
 export default () => ({
     isLoading: true,
-    entries: [1,2,3,4]
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Tempor commodo do elit incididunt exercitation sint dolore adipisicing nulla sunt Lorem aliquip. Dolor consectetur incididunt ipsum ipsum culpa commodo dolore. Laboris sit adipisicing nostrud ipsum mollit et. Dolor elit reprehenderit et incididunt aliquip tempor non eiusmod commodo. Nulla excepteur Lorem laboris anim et do incididunt aliquip. Minim irure magna nulla fugiat culpa non fugiat id. Fugiat dolore ad culpa non ipsum aliquip fugiat enim adipisicing mollit enim.',
+            picture: null, //url
+        },
+        {
+            id: new Date().getTime() + 100,
+            date: new Date().toDateString(),
+            text: 'Nulla velit aliquip amet minim. Enim aliquip pariatur do elit. Veniam nisi ut velit labore amet consectetur sunt reprehenderit. Aliquip aliqua ad officia magna culpa minim ea sunt enim esse in. Cupidatat commodo in minim mollit duis sunt enim elit.',
+            picture: null, //url
+        },
+        {
+            id: new Date().getTime() + 200,
+            date: new Date().toDateString(),
+            text: 'Adipisicing laborum officia cupidatat velit officia consectetur sunt. Ad deserunt tempor tempor veniam ipsum culpa est. Duis deserunt reprehenderit sit laboris id magna. Laborum occaecat occaecat cupidatat nisi commodo esse adipisicing in.',
+            picture: null, //url
+        },
+        {
+            id: new Date().getTime() + 300,
+            date: new Date().toDateString(),
+            text: 'Nulla et id velit non in quis enim non. Et eu adipisicing pariatur labore aute proident anim duis do esse. Anim aliqua enim laborum dolore amet amet cupidatat deserunt occaecat. Consectetur aute non do aliqua exercitation ipsum ex in dolor ullamco. Ipsum quis mollit tempor eu.',
+            picture: null, //url
+        }
+    ]
 })
